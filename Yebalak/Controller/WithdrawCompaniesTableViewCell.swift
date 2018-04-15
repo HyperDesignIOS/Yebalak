57bf9961322224850e7b5bae4d205a13f42d9f7d
HyperDesignIOS <gehadgamal045@gmail.com>
2018-04-15 17:12:11 +0200

    show withdraw companies

---
+++ Yebalak/Controller/WithdrawCompaniesTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  WithdrawCompaniesTableViewCell.swift
+//  Yebalak
+//
+//  Created by Hyper Design on 4/15/18.
+//  Copyright © 2018 AmrObjection. All rights reserved.
+//
+
+import UIKit
+
+class WithdrawCompaniesTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var withdrawCompanyLogo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
