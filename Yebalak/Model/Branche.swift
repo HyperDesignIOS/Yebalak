2708d374737f953f83a62390ed66cb49aa7b8118
AmrObjection <>
2018-04-16 08:40:35 -0700

    Deposit model and deposit history view controller

---
+++ Yebalak/Model/Branche.swift
@@ -0,0 +1,103 @@
+//
+//  Branche.swift
+//  Yebalak
+//
+//  Created by AmrObjection on 4/16/18.
+//  Copyright © 2018 AmrObjection. All rights reserved.
+//
+
+import Foundation
+
+
+class Branche : NSObject, NSCoding{
+    
+    var createdAt : String!
+    var id : Int!
+    var merchantId : String!
+    var nameAr : String!
+    var nameEn : String!
+    var updatedAt : String!
+    
+    
+    /**
+     * Instantiate the instance using the passed dictionary values to set the properties values
+     */
+    init(fromDictionary dictionary: [String:Any]){
+        createdAt = dictionary["created_at"] as? String
+        id = dictionary["id"] as? Int
+        merchantId = dictionary["merchant_id"] as? String
+        nameAr = dictionary["name_ar"] as? String
+        nameEn = dictionary["name_en"] as? String
+        updatedAt = dictionary["updated_at"] as? String
+    }
+    
+    /**
+     * Returns all the available property values in the form of [String:Any] object where the key is the approperiate json key and the value is the value of the corresponding property
+     */
+    func toDictionary() -> [String:Any]
+    {
+        var dictionary = [String:Any]()
+        if createdAt != nil{
+            dictionary["created_at"] = createdAt
+        }
+        if id != nil{
+            dictionary["id"] = id
+        }
+        if merchantId != nil{
+            dictionary["merchant_id"] = merchantId
+        }
+        if nameAr != nil{
+            dictionary["name_ar"] = nameAr
+        }
+        if nameEn != nil{
+            dictionary["name_en"] = nameEn
+        }
+        if updatedAt != nil{
+            dictionary["updated_at"] = updatedAt
+        }
+        return dictionary
+    }
+    
+    /**
+     * NSCoding required initializer.
+     * Fills the data from the passed decoder
+     */
+    @objc required init(coder aDecoder: NSCoder)
+    {
+        createdAt = aDecoder.decodeObject(forKey: "created_at") as? String
+        id = aDecoder.decodeObject(forKey: "id") as? Int
+        merchantId = aDecoder.decodeObject(forKey: "merchant_id") as? String
+        nameAr = aDecoder.decodeObject(forKey: "name_ar") as? String
+        nameEn = aDecoder.decodeObject(forKey: "name_en") as? String
+        updatedAt = aDecoder.decodeObject(forKey: "updated_at") as? String
+        
+    }
+    
+    /**
+     * NSCoding required method.
+     * Encodes mode properties into the decoder
+     */
+    @objc func encode(with aCoder: NSCoder)
+    {
+        if createdAt != nil{
+            aCoder.encode(createdAt, forKey: "created_at")
+        }
+        if id != nil{
+            aCoder.encode(id, forKey: "id")
+        }
+        if merchantId != nil{
+            aCoder.encode(merchantId, forKey: "merchant_id")
+        }
+        if nameAr != nil{
+            aCoder.encode(nameAr, forKey: "name_ar")
+        }
+        if nameEn != nil{
+            aCoder.encode(nameEn, forKey: "name_en")
+        }
+        if updatedAt != nil{
+            aCoder.encode(updatedAt, forKey: "updated_at")
+        }
+        
+    }
+    
+}
